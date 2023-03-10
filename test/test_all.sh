077d5f56ccc77357b8c784fc38a9a92ca17ccbda
devalio <github@devalio.ru>
2023-03-10 22:45:28 +0300

    added json option fot go test

--- test/test_all.sh
+++ test/test_all.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo "##teamcity[blockOpened name='Tests_Preparation']"
-go test -v --run=Test_fld_FillStatements . || echo "##teamcity[testFailed name='Tests_Preparation']"
+go test -json -v --run=Test_fld_FillStatements . || echo "##teamcity[testFailed name='Tests_Preparation']"
 echo "##teamcity[blockClosed name='Tests_Preparation']"
 
 echo "##teamcity[blockOpened name='Tests_Execution']"
 
 echo "##teamcity[testSuiteStarted name='generator.GeneratedCode']"
 cd ./test/tests/fill/ || exit 1
-go test -v ./... || echo "##teamcity[testFailed name='generator.GeneratedCode']"
+go test -json -v ./... || echo "##teamcity[testFailed name='generator.GeneratedCode']"
 echo "##teamcity[testSuiteFinished name='generator.GeneratedCode']"
 
 echo "##teamcity[testSuiteStarted name='generator.UnitTests']"
 cd ../../../generator || exit 1
-go test -v ./... || echo "##teamcity[testFailed name='generator.UnitTests']"
+go test -json -v ./... || echo "##teamcity[testFailed name='generator.UnitTests']"
 echo "##teamcity[testSuiteFinished name='generator.UnitTests']"
 
 echo "##teamcity[blockClosed name='Tests_Execution']"
