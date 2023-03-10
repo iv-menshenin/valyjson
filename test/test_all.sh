7f0e20bf82ad966f144860aba183355073f84e22
Igor Menshenin <igor@native.rent>
2023-03-10 22:29:26 +0300

    tests formatting

--- test/test_all.sh
+++ test/test_all.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-echo "GENERATE test code"
+echo "##teamcity[blockOpened name='Tests_Preparation']"
 go test -v --run=Test_fld_FillStatements .
+echo "##teamcity[blockClosed name='Tests_Preparation']"
 
-echo "TEST..."
+echo "##teamcity[blockOpened name='Tests_Execution']"
+
+echo "##teamcity[testSuiteStarted name='Execute_Test_Generation']"
 cd ./test/tests/fill/
 go test -v ./...
+echo "##teamcity[testSuiteFinished name='Execute_Test_Generation']"
+
+echo "##teamcity[testSuiteStarted name='Execute_Unit_Tests']"
+cd ../../../generator
+go test -v ./...
+echo "##teamcity[testSuiteFinished name='Execute_Unit_Tests']"
+
+echo "##teamcity[blockClosed name='Tests_Execution']"
