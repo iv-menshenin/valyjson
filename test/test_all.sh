019ad80e8e1f4e26fbe9e22503de2ad67ba7045c
devalio <github@devalio.ru>
2023-03-10 22:41:42 +0300

    added test fail messages

--- test/test_all.sh
+++ test/test_all.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo "##teamcity[blockOpened name='Tests_Preparation']"
-go test -v --run=Test_fld_FillStatements .
+go test -v --run=Test_fld_FillStatements . || echo "##teamcity[testFailed name='Tests_Preparation']"
 echo "##teamcity[blockClosed name='Tests_Preparation']"
 
 echo "##teamcity[blockOpened name='Tests_Execution']"
 
-echo "##teamcity[testSuiteStarted name='Execute_Test_Generation']"
-cd ./test/tests/fill/
-go test -v ./...
-echo "##teamcity[testSuiteFinished name='Execute_Test_Generation']"
+echo "##teamcity[testSuiteStarted name='generator.GeneratedCode']"
+cd ./test/tests/fill/ || exit 1
+go test -v ./... || echo "##teamcity[testFailed name='generator.GeneratedCode']"
+echo "##teamcity[testSuiteFinished name='generator.GeneratedCode']"
 
-echo "##teamcity[testSuiteStarted name='Execute_Unit_Tests']"
-cd ../../../generator
-go test -v ./...
-echo "##teamcity[testSuiteFinished name='Execute_Unit_Tests']"
+echo "##teamcity[testSuiteStarted name='generator.UnitTests']"
+cd ../../../generator || exit 1
+go test -v ./... || echo "##teamcity[testFailed name='generator.UnitTests']"
+echo "##teamcity[testSuiteFinished name='generator.UnitTests']"
 
 echo "##teamcity[blockClosed name='Tests_Execution']"
