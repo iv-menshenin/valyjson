9dfb3f2c408c8217941048bffefdca4328c0dcd0
devalio <github@devalio.ru>
2023-03-26 10:22:25 +0300

    arrays fulltested (97%)

--- test/tests/fill/test_slice/03_slice_fld.go.out_test.go
+++ test/tests/fill/test_slice/03_slice_fld.go.out_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_TestSlice_Unmarshal(t *testing.T) {
+func Test_TestSlice01_Unmarshal(t *testing.T) {
 	t.Run("test-all-empty", func(t *testing.T) {
 		var test1 TestSlice01
 		err := test1.UnmarshalJSON([]byte(`{"strs":[], "ints":null}`))
@@ -58,21 +58,30 @@ func Test_TestSlice_Unmarshal(t *testing.T) {
 	})
 }
 
-func Test_TestSlice_Unarshal_Bad(t *testing.T) {
+func Test_TestSlice_Unmarshal_Bad(t *testing.T) {
 	t.Run("empty-json", func(t *testing.T) {
 		var test1 TestSlice01
-		err := test1.UnmarshalJSON(nil)
-		require.Error(t, err)
+		require.Error(t, test1.UnmarshalJSON(nil))
+		var test2 TestSlice02
+		require.Error(t, test2.UnmarshalJSON(nil))
+		var test3 TestSlice03
+		require.Error(t, test3.UnmarshalJSON(nil))
 	})
 	t.Run("bad-json", func(t *testing.T) {
 		var test1 TestSlice01
-		err := test1.UnmarshalJSON([]byte{0, 1, 2, 3})
-		require.Error(t, err)
+		require.Error(t, test1.UnmarshalJSON([]byte{0, 1, 2, 3}))
+		var test2 TestSlice02
+		require.Error(t, test2.UnmarshalJSON([]byte{0, 1, 2, 3}))
+		var test3 TestSlice03
+		require.Error(t, test3.UnmarshalJSON([]byte{0, 1, 2, 3}))
 	})
 	t.Run("invalid-object", func(t *testing.T) {
 		var test1 TestSlice01
-		err := test1.UnmarshalJSON([]byte(`{"strs":null,"strs":null}`))
-		require.Error(t, err)
+		require.Error(t, test1.UnmarshalJSON([]byte(`{"strs":null,"strs":null}`)))
+		var test2 TestSlice02
+		require.Error(t, test2.UnmarshalJSON([]byte(`{}`)))
+		var test3 TestSlice03
+		require.Error(t, test3.UnmarshalJSON([]byte(`[]`)))
 	})
 	t.Run("invalid-object-field-strs", func(t *testing.T) {
 		var test1 TestSlice01
@@ -100,7 +109,7 @@ func Test_TestSlice_Unarshal_Bad(t *testing.T) {
 	})
 }
 
-func Test_TestSlice_Marshal(t *testing.T) {
+func Test_TestSlice01_Marshal(t *testing.T) {
 	t.Run("empty", func(t *testing.T) {
 		var s TestSlice01
 		b, err := s.MarshalJSON()
@@ -177,6 +186,16 @@ func TestTestSlice01_validate(t *testing.T) {
 		require.ErrorContains(t, err, "appears in the object twice")
 		require.ErrorContains(t, err, "strs")
 	})
+	t.Run("data_appears_twice", func(t *testing.T) {
+		const jData = `{"data":{},"data":{},"extended":{}}`
+		parser := jsonParserTestSlice01.Get()
+		v, err := parser.ParseBytes([]byte(jData))
+		require.NoError(t, err)
+		var test TestSlice03
+		err = test.validate(v, "(root)")
+		require.ErrorContains(t, err, "appears in the object twice")
+		require.ErrorContains(t, err, "data")
+	})
 	t.Run("both_appears_twice", func(t *testing.T) {
 		const jData = `{"strs":[],"ints":[],"strs":["1","2"],"ints":[1,2]}`
 		parser := jsonParserTestSlice01.Get()
@@ -188,17 +207,98 @@ func TestTestSlice01_validate(t *testing.T) {
 	})
 }
 
-func TestTestSlice01_IsZero(t *testing.T) {
+func TestTestSlice_IsZero(t *testing.T) {
 	t.Run("zero", func(t *testing.T) {
-		var test TestSlice01
-		require.True(t, test.IsZero())
+		var test1 TestSlice01
+		require.True(t, test1.IsZero())
+		var test2 TestSlice02
+		require.True(t, test2.IsZero())
+		var test3 TestSlice03
+		require.True(t, test3.IsZero())
 	})
-	t.Run("nonzero1", func(t *testing.T) {
-		var test = TestSlice01{Field: []string{}}
-		require.False(t, test.IsZero())
+	t.Run("nonzero", func(t *testing.T) {
+		var test1 = TestSlice01{Field: []string{}}
+		require.False(t, test1.IsZero())
+		var test1_2 = TestSlice01{FieldRef: []*int{}}
+		require.False(t, test1_2.IsZero())
+		var test2 = TestSlice02{TestSlice03{Data: 3}}
+		require.False(t, test2.IsZero())
+		var test3 = TestSlice03{Data: 44}
+		require.False(t, test3.IsZero())
 	})
-	t.Run("nonzero2", func(t *testing.T) {
-		var test = TestSlice01{FieldRef: []*int{}}
-		require.False(t, test.IsZero())
+}
+
+func Test_TestSlice02_Unmarshal(t *testing.T) {
+	t.Run("null", func(t *testing.T) {
+		var test1 TestSlice02
+		err := test1.UnmarshalJSON([]byte(`null`))
+		require.NoError(t, err)
+		require.Nil(t, test1)
+		require.Len(t, test1, 0)
+	})
+	t.Run("empty-array", func(t *testing.T) {
+		var test1 TestSlice02
+		err := test1.UnmarshalJSON([]byte(`[]`))
+		require.NoError(t, err)
+		require.NotNil(t, test1)
+		require.Len(t, test1, 0)
+	})
+	t.Run("filled-array", func(t *testing.T) {
+		var test1 TestSlice02
+		err := test1.UnmarshalJSON([]byte(`[{"data": 13},{"data": 54}]`))
+		require.NoError(t, err)
+		require.NotNil(t, test1)
+		require.Len(t, test1, 2)
+		require.Equal(t, TestSlice02{{Data: 13}, {Data: 54}}, test1)
+	})
+	t.Run("null-instead-of-object", func(t *testing.T) {
+		var test1 TestSlice02
+		err := test1.UnmarshalJSON([]byte(`[{"data": 13},null]`))
+		require.Error(t, err)
+		require.ErrorContains(t, err, "value doesn't contain object")
+	})
+	t.Run("filled-array-2", func(t *testing.T) {
+		var test1 TestSlice02
+		err := test1.UnmarshalJSON([]byte(`[{"data": 13},{},{"data": 54}]`))
+		require.NoError(t, err)
+		require.NotNil(t, test1)
+		require.Len(t, test1, 3)
+		require.Equal(t, TestSlice02{{Data: 13}, {}, {Data: 54}}, test1)
+	})
+}
+
+func Test_TestSlice02_Marshal(t *testing.T) {
+	t.Run("null", func(t *testing.T) {
+		var test TestSlice02
+		data, err := test.MarshalJSON()
+		require.NoError(t, err)
+		require.JSONEq(t, "null", string(data))
+	})
+	t.Run("empty", func(t *testing.T) {
+		var test = TestSlice02{}
+		data, err := test.MarshalJSON()
+		require.NoError(t, err)
+		require.JSONEq(t, "[]", string(data))
+	})
+	t.Run("marshal", func(t *testing.T) {
+		var test = TestSlice02{{Data: 190}, {Data: 191}}
+		data, err := test.MarshalJSON()
+		require.NoError(t, err)
+		require.JSONEq(t, `[{"data":190},{"data":191}]`, string(data))
+	})
+}
+
+func Test_TestSlice03_Marshal(t *testing.T) {
+	t.Run("null", func(t *testing.T) {
+		var test *TestSlice03
+		data, err := test.MarshalJSON()
+		require.NoError(t, err)
+		require.JSONEq(t, "null", string(data))
+	})
+	t.Run("empty", func(t *testing.T) {
+		var test = TestSlice03{}
+		data, err := test.MarshalJSON()
+		require.NoError(t, err)
+		require.JSONEq(t, `{"data":0}`, string(data))
 	})
 }
