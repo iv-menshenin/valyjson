3503c6155cae72e70cb275c537b69358bd36d7b6
devalio <github@devalio.ru>
2023-04-29 14:14:51 +0300

    test regeneration status checking added

--- test/fields_test.go
+++ test/fields_test.go
@@ -1,8 +1,13 @@
 package test
 
 import (
-	"io/ioutil"
+	"crypto/md5" //nolint:gosec // it`s not for security
+	"fmt"
+	"io"
+	"os"
 	"path"
+	"reflect"
+	"sort"
 	"strings"
 	"testing"
 
@@ -12,7 +17,8 @@ import (
 const fillTestsDir = "./tests/fill/"
 
 func Test_fld_FillStatements(t *testing.T) {
-	dirs, err := ioutil.ReadDir(fillTestsDir)
+	hashes1 := hashDir(fillTestsDir)
+	dirs, err := os.ReadDir(fillTestsDir)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -20,10 +26,11 @@ func Test_fld_FillStatements(t *testing.T) {
 	var cnt int
 	for _, d := range dirs {
 		if !d.IsDir() {
+			sort.Strings(hashes1)
 			continue
 		}
 		dirPath := path.Join(fillTestsDir, d.Name())
-		files, err := ioutil.ReadDir(dirPath)
+		files, err := os.ReadDir(dirPath)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -50,6 +57,12 @@ func Test_fld_FillStatements(t *testing.T) {
 	if cnt == 0 {
 		t.Error("there is no tests")
 	}
+	hashes2 := hashDir(fillTestsDir)
+	sort.Strings(hashes1)
+	sort.Strings(hashes2)
+	if !reflect.DeepEqual(hashes1, hashes2) {
+		t.Error("tests are not regenerated")
+	}
 }
 
 func caseTestFillStatements(t *testing.T, testFile string) {
@@ -69,4 +82,40 @@ func caseTestFillStatements(t *testing.T, testFile string) {
 
 func Test_GenerateVJson(t *testing.T) {
 	caseTestFillStatements(t, "./vjson/types.go")
+}
+
+func hashDir(dirPath string) []string {
+	dirs, err := os.ReadDir(dirPath)
+	if err != nil {
+		panic(err)
+	}
+
+	var hashes = make([]string, 0)
+	for _, d := range dirs {
+		if !d.IsDir() {
+			continue
+		}
+		subDirPath := path.Join(dirPath, d.Name())
+		files, err := os.ReadDir(subDirPath)
+		if err != nil {
+			panic(err)
+		}
+		for _, f := range files {
+			hashes = append(hashes, hashFile(path.Join(subDirPath, f.Name())))
+		}
+	}
+	return hashes
+}
+
+func hashFile(fileName string) string {
+	f, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	h := md5.New()
+	if _, err = io.Copy(h, f); err != nil {
+		panic(err)
+	}
+	var buf [md5.Size]byte
+	return fmt.Sprintf("%X", h.Sum(buf[:0]))
 }
